f6e780c4e5ecae02da050cd5094bf86ce2f0d743
IrinaButyugova <irina.butyugova@sharp-dev.net>
2024-01-11 15:58:56 +0300

    Routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  BrowserRouter,
+  Routes
+} from "react-router-dom";
+import Login from "./Login"
+import Main from "./Main";
+import About from "./About";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <h1>Simple SPA</h1>
+          <ul className="header">
+            <li><NavLink to="/">Main</NavLink></li>
+            <li><NavLink to="/login">Login</NavLink></li>
+            <li><NavLink to="/about">About</NavLink></li>
+          </ul>
+          <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Main/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/about" element={<About/>}/>
+          </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/Main.js
@@ -0,0 +1,5 @@
+export default function Main() {
+    return (
+      <span>Main page</span>
+    );
+  }
